60e10c530f36d6a878600d7a7cf9b164283280ce
ecnopdev <ponce.josemari@gmail.com>
2020-06-01 17:20:22 +0800

    add pages and assets

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,65 @@
 <template>
   <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="transparent" flat>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('@/assets/logo3.svg')"
           transition="scale-transition"
           width="40"
         />
       </div>
 
       <v-spacer></v-spacer>
-    </v-app-bar> -->
 
-    <v-content>
-      <HelloWorld/>
+      <div class="d-flex flex-row nav-links align-center">
+        <div>
+          <span>01.</span> About
+        </div>
+        <div>
+          <span>02.</span> Experience
+        </div>
+        <div>
+          <span>03.</span> Work
+        </div>
+        <div>
+          <span>04.</span> Contact
+        </div>
+        <v-btn class="ml-3" green outlined href="Jose-Mari-Ponce-resume.pdf" target="_bank">
+          Resume
+        </v-btn>
+      </div>
+    </v-app-bar>
+
+    <v-content style="scroll-snap-type:y proximity">
+      <Home />
     </v-content>
   </v-app>
 </template>
 
+<style scoped>
+.nav-links div {
+  margin-left: 16px;
+}
+.nav-links span {
+  color: green;
+  font-weight: bold;
+}
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld';
+import Home from "./components/Home";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    Home
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container fluid style="scroll-snap-align:start;height:100vh">
+    <v-row>
+      <v-col cols="2"></v-col>
+      <v-col cols="8">
+        <v-row>
+          <v-col class="mb-4">
+            <slot name="title"></slot>
+            <div class="title green--text font-weight-bold mb-4">Hi, my name is</div>
+
+            <h1 class="display-3 font-weight-bold mb-3">Jose Mari Ponce</h1>
+
+            <h2 class="display-3 font-weight-bold mb-5 grey--text">I build things for the web.</h2>
+
+            <p class="title font-weight-regular" style="max-width:450px">
+              I'm a
+              <span class="font-weight-bold green--text">full-stack software engineer</span>
+              , an amateur multi-instrumentalist,
+              a gamer,
+              and a dad who finds ways to make the best of all worlds.
+            </p>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="2" align="right" justify="bottom" align-self="end"></v-col>
+    </v-row>
+    <div class="email-address"><a href="mailto:ponce.josemari@gmail.com">ponce.josemari@gmail.com</a> --------------------------</div>
+  </v-container>
+</template>
+
+<style scoped>
+.email-address {
+  writing-mode: vertical-rl;
+  text-orientation: mixed;
+  width: 45px;
+  height: 400px;
+  display: block;
+  position: fixed;
+  right: 0px;
+  bottom: 0px;
+  padding-right: 16px;
+}
+.email-address a {
+  text-decoration: none;
+}
+</style>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader"
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify"
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify"
+      }
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com"
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com"
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify"
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs"
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify"
+      }
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ]
+  })
+};
+</script>
